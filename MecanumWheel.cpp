bbdb077dd47df5043f53c6b326ed2dc9c37b4f7d
EthanShao1 <goduck92@gmail.com>
2025-01-09 19:58:10 +0800

    調整 MecanumWheel 類別以支援三個引腳，修正移動邏輯以正確控制馬達方向

--- MecanumWheel.cpp
+++ MecanumWheel.cpp
@@ -1,11 +1,11 @@
 #include "MecanumWheel.h"
 
-MecanumWheel::MecanumWheel(int frontLeft[2], int frontRight[2], int backLeft[2], int backRight[2]) {
+MecanumWheel::MecanumWheel(int frontLeft[3], int frontRight[3], int backLeft[3], int backRight[3]) {
     memcpy(_frontLeft, frontLeft, sizeof(_frontLeft));
     memcpy(_frontRight, frontRight, sizeof(_frontRight));
     memcpy(_backLeft, backLeft, sizeof(_backLeft));
     memcpy(_backRight, backRight, sizeof(_backRight));
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 3; i++) {
         pinMode(_frontLeft[i], OUTPUT);
         pinMode(_frontRight[i], OUTPUT);
         pinMode(_backLeft[i], OUTPUT);
@@ -14,10 +14,10 @@ MecanumWheel::MecanumWheel(int frontLeft[2], int frontRight[2], int backLeft[2],
 }
 
 void MecanumWheel::move(float xSpeed, float ySpeed, float rotation) {
-    float frontLeftSpeed = ySpeed + xSpeed - rotation;
-    float frontRightSpeed = ySpeed - xSpeed + rotation;
-    float backLeftSpeed = ySpeed - xSpeed - rotation;
-    float backRightSpeed = ySpeed + xSpeed + rotation;
+    float frontLeftSpeed = ySpeed + xSpeed + rotation;
+    float frontRightSpeed = ySpeed - xSpeed - rotation;
+    float backLeftSpeed = ySpeed - xSpeed + rotation;
+    float backRightSpeed = ySpeed + xSpeed - rotation;
 
     setMotor(_frontLeft, frontLeftSpeed);
     setMotor(_frontRight, frontRightSpeed);
@@ -32,14 +32,20 @@ void MecanumWheel::stop() {
     setMotor(_backRight, 0);
 }
 
-void MecanumWheel::setMotor(int motor[2], float speed) {
-    int dirPin = motor[0];
-    int pwmPin = motor[1];
+void MecanumWheel::setMotor(int motor[3], float speed) {
+    int dirPin1 = motor[0];
+    int dirPin2 = motor[1];
+    int pwmPin = motor[2];
     
     if (speed > 0) {
-        digitalWrite(dirPin, HIGH);
+        digitalWrite(dirPin1, HIGH);
+        digitalWrite(dirPin2, LOW);
+    } else if (speed == 0) {
+        digitalWrite(dirPin1, LOW);
+        digitalWrite(dirPin2, LOW);
     } else {
-        digitalWrite(dirPin, LOW);
+        digitalWrite(dirPin1, LOW);
+        digitalWrite(dirPin2, HIGH);
         speed = -speed;
     }
     analogWrite(pwmPin, constrain(speed, 0, 255));
--- MecanumWheel.h
+++ MecanumWheel.h
@@ -5,17 +5,17 @@
 
 class MecanumWheel {
 public:
-    MecanumWheel(int frontLeft[2], int frontRight[2], int backLeft[2], int backRight[2]);
+    MecanumWheel(int frontLeft[3], int frontRight[3], int backLeft[3], int backRight[3]);
     void move(float xSpeed, float ySpeed, float rotation);
     void stop();
 
 private:
-    int _frontLeft[2];
-    int _frontRight[2];
-    int _backLeft[2];
-    int _backRight[2];
+    int _frontLeft[3];
+    int _frontRight[3];
+    int _backLeft[3];
+    int _backRight[3];
     
-    void setMotor(int motor[2], float speed);
+    void setMotor(int motor[3], float speed);
 };
 
 #endif
